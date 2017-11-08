984a6e40f531ac76585bd09cc87801301da65fd4
Santiago Mendez <sanjmen@gmail.com>
2017-11-08 02:47:32 -0300

    added modules, components and configured application for dependency injection

--- app/src/main/java/com/sanjmen/simplecomics/ComicApp.java
+++ app/src/main/java/com/sanjmen/simplecomics/ComicApp.java
@@ -3,12 +3,21 @@ package com.sanjmen.simplecomics;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.sanjmen.simplecomics.di.AppComponent;
+import com.sanjmen.simplecomics.di.DaggerAppComponent;
 
+import dagger.android.AndroidInjector;
+import dagger.android.DaggerApplication;
 import timber.log.Timber;
 
 import static timber.log.Timber.DebugTree;
 
-public class ComicApp extends Application {
+/**
+ * We create a custom {@link Application} class that extends  {@link DaggerApplication}.
+ * We then override applicationInjector() which tells Dagger how to make our @Singleton Component
+ * We never have to call `component.inject(this)` as {@link DaggerApplication} will do that for us.
+ */
+public class ComicApp extends DaggerApplication {
 
     @Override
     public void onCreate() {
@@ -18,5 +27,12 @@ public class ComicApp extends Application {
             Timber.plant(new DebugTree());
             Stetho.initializeWithDefaults(this);
         }
+    }
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        AppComponent appComponent = DaggerAppComponent.builder().application(this).build();
+        appComponent.inject(this);
+        return appComponent;
     }
 }
